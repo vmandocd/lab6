fadd49c8505b1da5451bf8c6187da9d4d07ee8f7
Von Jhiro Mandocdoc <vmandocd@ucsd.edu>
2016-02-11 20:28:19 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,6 +26,9 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
+	console.log("/project/" + idNumber);
+	$.get("/project/" + idNumber, emptyCallback);
+
 	console.log("User clicked on project " + idNumber);
 }
 
@@ -35,4 +38,25 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette", changeColor);
+}
+
+function emptyCallback(result) {
+	console.log(result);
+
+	var callbackHTML = '<a href="#" class="thumbnail">' + '<img src="' + result['image'] + '"class="detailsImage">' + '<p>' + result['title'] + '</p>' + '<p><small>' + result['date'] + '</small></p></a>' + '<div class="details">' + '<p>' + result['summary'] + '</p></div>';
+
+	$("#project" + result.id + " .details").html(callbackHTML);
+}
+
+function changeColor(result) {
+
+	var colors = result.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
